cb731f39d72d025cd309f9a931b528f45275b675
arbitrilion <arbitrilion@163.com>
2020-07-11 13:31:55 +0800

    add ex8.3 and 7.8

---
+++ ch7/ex7.8/stateful_sort.go
@@ -0,0 +1,140 @@
+// 练习7.8:
+// 很多图形界面提供了一个表格控件，它支持有状态的多层排序：
+// 先按照最近单击的列来排序，接着是上一次单击的列，以此类推。
+// 请定义 sort.Interface 接口实现来满足如上需求。
+// 试比较这个方法与多次使用 sort.Stable 排序的异同。
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"text/tabwriter"
+	"time"
+)
+
+//!+main
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+//!-main
+
+//!+printTracks
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush() // calculate column widths and print table
+}
+
+//!-printTracks
+
+//!+artistcode
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+//!-artistcode
+
+//!+yearcode
+type byYear []*Track
+
+func (x byYear) Len() int           { return len(x) }
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+//!-yearcode
+
+//!+customcode
+type customSort struct {
+	t    []*Track
+	less func(x, y *Track) bool
+}
+
+func (x customSort) Len() int           { return len(x.t) }
+func (x customSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
+func (x customSort) Swap(i, j int)      { x.t[i], x.t[j] = x.t[j], x.t[i] }
+
+func main() {
+
+	fmt.Println("\nCustom:")
+	//!+customcall
+	sort.Sort(customSort{tracks, func(x, y *Track) bool {
+		if x.Title != y.Title {
+			return x.Title < y.Title
+		}
+		if x.Year != y.Year {
+			return x.Year < y.Year
+		}
+		if x.Length != y.Length {
+			return x.Length < y.Length
+		}
+		return false
+	}})
+	//!-customcall
+	printTracks(tracks)
+}
+
+/*
+//!+artistoutput
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+Go          Delilah         From the Roots Up  2012  3m38s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+Go          Moby            Moby               1992  3m37s
+//!-artistoutput
+
+//!+artistrevoutput
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go          Moby            Moby               1992  3m37s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+Go          Delilah         From the Roots Up  2012  3m38s
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+//!-artistrevoutput
+
+//!+yearoutput
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go          Moby            Moby               1992  3m37s
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+Go          Delilah         From the Roots Up  2012  3m38s
+//!-yearoutput
+
+//!+customout
+Title       Artist          Album              Year  Length
+-----       ------          -----              ----  ------
+Go          Moby            Moby               1992  3m37s
+Go          Delilah         From the Roots Up  2012  3m38s
+Go Ahead    Alicia Keys     As I Am            2007  4m36s
+Ready 2 Go  Martin Solveig  Smash              2011  4m24s
+//!-customout
+*/
